8864fddcbab053afdaacddb045f8592a7b82af4a
Bradley V <bvenant@aol.com>
2022-05-21 18:18:08 -0400

    added assets folder

---
+++ assets/js/script.js
@@ -0,0 +1,92 @@
+[
+    {
+        "id":1,
+        "question":"what is the question?",
+        "a":"boolean",
+        "b":"true",
+        "c":"false",
+        "d":"type",
+        "answer":"a"
+    },
+    {
+        "id":2,
+        "question":"what is the question?",
+        "a":"boolean",
+        "b":"true",
+        "c":"false",
+        "d":"type",
+        "answer":"a"
+    },
+    {
+        "id":3,
+        "question":"what is the question?",
+        "a":"boolean",
+        "b":"true",
+        "c":"false",
+        "d":"type",
+        "answer":"a"
+    },
+    {
+        "id":4,
+        "question":"what is the question?",
+        "a":"boolean",
+        "b":"true",
+        "c":"false",
+        "d":"type",
+        "answer":"a"
+    },
+    {
+        "id":5,
+        "question":"what is the question?",
+        "a":"boolean",
+        "b":"true",
+        "c":"false",
+        "d":"type",
+        "answer":"a"
+    },
+    {
+        "id":6,
+        "question":"what is the question?",
+        "a":"boolean",
+        "b":"true",
+        "c":"false",
+        "d":"type",
+        "answer":"a"
+    },
+    {
+        "id":7,
+        "question":"what is the question?",
+        "a":"boolean",
+        "b":"true",
+        "c":"false",
+        "d":"type",
+        "answer":"a"
+    },
+    {
+        "id":8,
+        "question":"what is the question?",
+        "a":"boolean",
+        "b":"true",
+        "c":"false",
+        "d":"type",
+        "answer":"a"
+    },
+    {
+        "id":9,
+        "question":"what is the question?",
+        "a":"boolean",
+        "b":"true",
+        "c":"false",
+        "d":"type",
+        "answer":"a"
+    },
+    {
+        "id":10,
+        "question":"what is the question?",
+        "a":"boolean",
+        "b":"true",
+        "c":"false",
+        "d":"type",
+        "answer":"a"
+    },
+]
