577d8a25515f89d29a0ec476dd652e63c54818f0
Bradley V <bvenant@aol.com>
2022-05-25 12:37:57 -0400

    add questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,92 +1,74 @@
-[
+var questions = [
     {
         "id":1,
-        "question":"what is the question?",
-        "a":"boolean",
-        "b":"true",
-        "c":"false",
-        "d":"type",
-        "answer":"a"
+        "question":"JavaScript is the programming language of the __.",
+        "A":"Desktop",
+        "B":"Mobile",
+        "C":"Web",
+        "D":"Server",
+        "answer":"C"
     },
     {
         "id":2,
-        "question":"what is the question?",
-        "a":"boolean",
-        "b":"true",
-        "c":"false",
-        "d":"type",
-        "answer":"a"
+        "question":"Which type of JavaScript Language is __?",
+        "A":"Object-oriented",
+        "B":"Object-based",
+        "C":"Functional programming",
+        "D":"All of the above",
+        "answer":"b"
     },
     {
         "id":3,
-        "question":"what is the question?",
-        "a":"boolean",
-        "b":"true",
-        "c":"false",
-        "d":"type",
-        "answer":"a"
+        "question":"In which HTML element, we put the JavaScript code?",
+        "A":"<javascript>...</javascript>",
+        "B":"<js>...</js>",
+        "C":"<script>...</script>",
+        "D":"<css>...</css>",
+        "answer":"C"
     },
     {
         "id":4,
-        "question":"what is the question?",
-        "a":"boolean",
-        "b":"true",
-        "c":"false",
-        "d":"type",
-        "answer":"a"
+        "question":"JavaScript code can be written in ____.",
+        "A":"JavaScript file (.js file)",
+        "B":"JavaScript file and in HTML document directly",
+        "C":"HTML document directly",
+        "D":"In style sheets (.css file)",
+        "answer":"B"
     },
     {
         "id":5,
-        "question":"what is the question?",
-        "a":"boolean",
-        "b":"true",
-        "c":"false",
-        "d":"type",
-        "answer":"a"
+        "question":"Which symbol is used separate JavaScript statements?",
+        "A":"Comma (,)",
+        "B":"Colon (:)",
+        "C":"Hyphen (_)",
+        "D":"Semicolon (;)",
+        "answer":"D"
     },
     {
         "id":6,
-        "question":"what is the question?",
-        "a":"boolean",
-        "b":"true",
-        "c":"false",
-        "d":"type",
-        "answer":"a"
+        "question":"JavaScript ignores?",
+        "A":"newlines",
+        "B":"tabs",
+        "C":"spaces",
+        "D":"All of the above",
+        "answer":"D"
     },
     {
         "id":7,
-        "question":"what is the question?",
-        "a":"boolean",
-        "b":"true",
-        "c":"false",
-        "d":"type",
-        "answer":"a"
+        "question":"Which JavaScript method is used to access an HTML element by id?",
+        "A":"getElementById()",
+        "B":"getElement(id)",
+        "C":"getElementById(id)",
+        "D":"elementById(id)",
+        "answer":"C"
     },
     {
         "id":8,
-        "question":"what is the question?",
-        "a":"boolean",
-        "b":"true",
-        "c":"false",
-        "d":"type",
-        "answer":"a"
-    },
-    {
-        "id":9,
-        "question":"what is the question?",
-        "a":"boolean",
-        "b":"true",
-        "c":"false",
-        "d":"type",
-        "answer":"a"
-    },
-    {
-        "id":10,
-        "question":"what is the question?",
-        "a":"boolean",
-        "b":"true",
-        "c":"false",
-        "d":"type",
-        "answer":"a"
+        "question":"Which JavaScript method is used to write HTML output?",
+        "A":"document.write()",
+        "B":"document.output()",
+        "C":"console.log()",
+        "D":"document.writeHTML()",
+        "answer":"A"
     },
 ]
