d2c64f22c78ba1d98e7edbadea58f698249efc6f
Bradley V <bvenant@aol.com>
2022-05-25 15:29:57 -0400

    add variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,96 @@
 var questions = [
     {
-        "id":1,
-        "question":"JavaScript is the programming language of the __.",
-        "A":"Desktop",
-        "B":"Mobile",
-        "C":"Web",
-        "D":"Server",
-        "answer":"C"
+        question: "JavaScript is the programming language of the __.",
+        options: [
+        "A. Desktop",
+        "B. Mobile",
+        "C. Web",
+        "D. Server",
+        ],
+        answer: "C"
     },
     {
-        "id":2,
-        "question":"Which type of JavaScript Language is __?",
-        "A":"Object-oriented",
-        "B":"Object-based",
-        "C":"Functional programming",
-        "D":"All of the above",
-        "answer":"b"
+        question: "Which type of JavaScript Language is __?",
+        options: [
+        "A. Object-oriented",
+        "B. Object-based",
+        "C. Functional programming",
+        "D. All of the above",
+        ],
+        answer:"b"
     },
     {
-        "id":3,
-        "question":"In which HTML element, we put the JavaScript code?",
-        "A":"<javascript>...</javascript>",
-        "B":"<js>...</js>",
-        "C":"<script>...</script>",
-        "D":"<css>...</css>",
-        "answer":"C"
+        question: "In which HTML element, we put the JavaScript code?",
+        options: [
+        "A. <javascript>...</javascript>",
+        "B. <js>...</js>",
+        "C. <script>...</script>",
+        "D. <css>...</css>",
+        ],
+        answer: "C"
     },
     {
-        "id":4,
-        "question":"JavaScript code can be written in ____.",
-        "A":"JavaScript file (.js file)",
-        "B":"JavaScript file and in HTML document directly",
-        "C":"HTML document directly",
-        "D":"In style sheets (.css file)",
-        "answer":"B"
+        question: "JavaScript code can be written in ____.",
+        options: [
+        "A. JavaScript file (.js file)",
+        "B. JavaScript file and in HTML document directly",
+        "C. HTML document directly",
+        "D. In style sheets (.css file)",
+        ],
+        answer: "B"
     },
     {
-        "id":5,
-        "question":"Which symbol is used separate JavaScript statements?",
-        "A":"Comma (,)",
-        "B":"Colon (:)",
-        "C":"Hyphen (_)",
-        "D":"Semicolon (;)",
-        "answer":"D"
+        question: "Which symbol is used separate JavaScript statements?",
+        options: [
+        "A. Comma (,)",
+        "B. Colon (:)",
+        "C. Hyphen (_)",
+        "D. Semicolon (;)",
+        ],
+        answer: "D"
     },
     {
-        "id":6,
-        "question":"JavaScript ignores?",
-        "A":"newlines",
-        "B":"tabs",
-        "C":"spaces",
-        "D":"All of the above",
-        "answer":"D"
+        question: "JavaScript ignores?",
+        options: [
+        "A. newlines",
+        "B. tabs",
+        "C. spaces",
+        "D. All of the above",
+        ],
+        answer: "D"
     },
     {
-        "id":7,
-        "question":"Which JavaScript method is used to access an HTML element by id?",
-        "A":"getElementById()",
-        "B":"getElement(id)",
-        "C":"getElementById(id)",
-        "D":"elementById(id)",
-        "answer":"C"
+        question: "Which JavaScript method is used to access an HTML element by id?",
+        options: [
+        "A. getElementById()",
+        "B. getElement(id)",
+        "C. getElementById(id)",
+        "D. elementById(id)",
+        ],
+        answer: "C"
     },
     {
-        "id":8,
-        "question":"Which JavaScript method is used to write HTML output?",
-        "A":"document.write()",
-        "B":"document.output()",
-        "C":"console.log()",
-        "D":"document.writeHTML()",
-        "answer":"A"
+        question: "Which JavaScript method is used to write HTML output?",
+        options: [
+        "A. document.write()",
+        "B. document.output()",
+        "C. console.log()",
+        "D. document.writeHTML()",
+        ],
+        answer: "A"
     },
-]
+];
+
+var answer = document.querySelector("#answer");
+
+//Score 
+var highScore = document.querySelector("#highScore");
+var scoreList = document.querySelector("#scoreList");
+var finalScore = document.querySelector("#finalScore");
+
+// Time 
+var timer = document.querySelector("#timer");
+
+// Start Quiz 
+var start = document.querySelector("#start");
+
